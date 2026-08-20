35412a05cbbc189cda3767b3a3010c40214a2806
Brian Fernandez <141482327+brianmfdz@users.noreply.github.com>
2026-08-20 13:19:52 +0530

    Start project development

---
+++ celestial_body.py
@@ -0,0 +1,4 @@
+class CelestialBody:
+    def __init__(self, CelesName, CelesPosition):
+        self.CelesName = CelesName
+        self.CelesPosition = CelesPosition
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 print("COMETRA is starting...")
+
+from celestial_body import CelestialBody
+
+sun = CelestialBody("Sun", (0, 0, 0))
+earth = CelestialBody("Earth", (1, 0, 0))
+
+print(sun.CelesName)
+print(earth.CelesName)
